3dbbeadcf7b2619b112496f3c68b5f7cc942691e
mos-jef <anarchismo30@gmail.com>
2025-07-27 09:33:01 -0700

    Deploy debug build to test error messages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1801,7 +1801,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "569ac2245f5958610ecda55462936818
 "favicon.png": "13ff9ae0ec38ed5ef9b5319596ccab3e",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "flutter.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "1b253ebc5a6d30f15085fd87267a8e78",
+"flutter_bootstrap.js": "d6298d6e4d3e0d8b8f6640b42faba147",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
